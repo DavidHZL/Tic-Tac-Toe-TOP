f3c368d9f7be2d5e1380263d44908b0f6f360063
DavidHZL <hanzldavid707@gmail.com>
2022-02-21 01:27:28 -0600

    Full Functionality for game, minus design

--- script.js
+++ script.js
@@ -1,60 +1,168 @@
-const gameBoard = (() => {
-    var board = [];
-
-    function startGame() {
-        var gBoard = document.getElementById('gBoard');
-        
-        for (let i = 0; i < 9; i++) {
-            var li = document.createElement('li');
-            li.classList.add('tile');
-            li.textContent = board[i];
-            gBoard.append(li);
-        }
-    }
-
-    document.getElementById('gBoard').onclick = function(event) {
-        var target = getEventTarget(event);
-        board.push(david.prototype.getMarker());
-        target.textContent = david.prototype.getMarker();
-        console.log(board);
-    }
-
-    return {
-        startGame: startGame 
-    };
-})();
+const intro = document.querySelector('.intro');
+const introTitle = document.querySelector('.intro-title');
+const controlBox = document.querySelector('.controlBox');
+const gameScreen = document.querySelector('.gameScreen');
 
 const Player = (marker) => {
     
-    const getMarker = () => {return marker;};
+    const getSign = () => {return marker;};
 
-    const setMarker = (newMarker) => {marker = newMarker};
-
-    return {getMarker: getMarker,
-            setMarker: setMarker};
+    return {getSign: getSign};
 }
 
-const newHuman = (marker) => {
-    const prototype = Player(marker);
+const gameBoard = (() => {
+    var board = ['', '', '', '', '', '', '', '', ''];
 
-    return {prototype}
-}
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        board[index] = sign;
+    };
 
-const newComputer = (marker) => {
-    const prototype = Player(marker);
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
 
-    return {prototype};
-}
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+        board[i] = "";
+        }
+    };
 
-const david = newHuman('X');
-const comp = newComputer('O');
+    return {setField, getField, reset};
+})();
 
-gameBoard.startGame();
+const displayController = (() => {
+    const messageElement = document.getElementById('message');
+    const resetButton = document.getElementById('reset');
 
-console.log(david.prototype.getMarker());
+    const startUp = function() {
+        gameScreen.style.display = 'none';
+    
+        introTitle.textContent = 'Tic Tac Toe';
+        var newBtn = document.createElement('input');
+    
+        newBtn.type = 'button';
+        newBtn.classList.add('controlBoxBtn');
+        newBtn.setAttribute('id', 'start-game');
+        newBtn.onclick = function() {
+            intro.style.display = 'none';
+            gameScreen.style.display = 'grid';
+            buildBoard();
+        };
+        newBtn.value = 'Start Game';
+    
+        controlBox.append(newBtn);
+    } 
 
+    const buildBoard = () => {
+        var gBoard = document.getElementById('gBoard');
 
-function getEventTarget(e) {
-    e = e || window.event;
-    return e.target || e.srcElement; 
-}
+        for(let i = 0; i < 9; i++){
+            var newDiv = document.createElement('div');
+            newDiv.classList.add('tile');
+            newDiv.setAttribute('id', 'tile');
+            newDiv.dataset.index = i;
+            newDiv.addEventListener("click", (e) => {
+                if (gameController.getIsOver() || e.target.textContent != '') return;
+                gameController.playRound(parseInt(e.target.dataset.index));
+                updateGameBoard();
+            })
+            gBoard.append(newDiv);
+        }
+    }
+
+    resetButton.addEventListener("click", (e) => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameBoard();
+        setMessageElement("Player X's turn");
+    })
+
+    
+    const updateGameBoard = () => {
+        let tileElements = document.querySelectorAll('#tile');
+        console.log(tileElements);
+        for(let i = 0; i < tileElements.length; i++){
+            tileElements[i].textContent = gameBoard.getField(i);
+        }
+    };
+
+    const setResultMessage = (winner) => {
+        if (winner === "Draw") {
+          setMessageElement("It's a draw!");
+        } else {
+          setMessageElement(`Player ${winner} has won!`);
+        }
+      };
+    
+    const setMessageElement = (message) => {
+        messageElement.textContent = message;
+    };
+    
+    return { setResultMessage, setMessageElement, startUp };
+})();
+
+const gameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+        if (checkWinner(fieldIndex)) {
+            displayController.setMessageElement(getCurrentPlayerSign());
+            isOver = true;
+            return;
+        }
+        if (round === 9) {
+            displayController.setResultMessage("Draw");
+            isOver = true;
+            return;
+        }
+        round++;
+        displayController.setMessageElement(
+            `Player ${getCurrentPlayerSign()}'s Turn`
+        );
+    };
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+      };
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+    
+        return winConditions
+          .filter((combination) => combination.includes(fieldIndex))
+          .some((possibleCombination) =>
+            possibleCombination.every(
+              (index) => gameBoard.getField(index) === getCurrentPlayerSign()
+            )
+          );
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    }
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    }
+
+    
+    return { playRound, getIsOver, reset };
+})();
+
+displayController.startUp();
